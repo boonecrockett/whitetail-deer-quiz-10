da8f2b9cd77a5d8a9dafb2f5b8e8f7052a8f7b6a
boonecrockett <97560768+boonecrockett@users.noreply.github.com>
2024-08-15 08:33:08 -0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import QuizApp from './QuizApp';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <QuizApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
