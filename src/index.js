fa4be4484be6f1ee8877c6ed559b74389096e8e1
boonecrockett <97560768+boonecrockett@users.noreply.github.com>
2024-08-15 08:50:08 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import QuizApp from './QuizApp';
+import ErrorBoundary from './ErrorBoundary';
 
 ReactDOM.render(
   <React.StrictMode>
-    <QuizApp />
+    <ErrorBoundary>
+      <QuizApp />
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
